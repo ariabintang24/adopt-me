c6dfbd46910514acfe155217d45dfb57aeadbc7c
Aria Bintang <ariabintang24@gmail.com>
2026-02-26 20:18:19 +0700

    add functionallity users resource for admin panel

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -3,12 +3,19 @@
 namespace App\Filament\Resources\Users\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+
+
 
 class UsersTable
 {
@@ -16,13 +23,29 @@ class UsersTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('email')->searchable(),
+                TextColumn::make('phone'),
+                BadgeColumn::make('status')
+                    ->label('Status')
+                    ->getStateUsing(
+                        fn($record) =>
+                        $record->deleted_at ? 'Inactive' : 'Active'
+                    )
+                    ->colors([
+                        'success' => 'Active',
+                        'danger' => 'Inactive',
+                    ]),
+                TextColumn::make('created_at')->dateTime()->sortable(),
             ])
             ->filters([
                 TrashedFilter::make(),
             ])
             ->recordActions([
-                EditAction::make(),
+                ViewAction::make(),
+                DeleteAction::make(),
+                RestoreAction::make(),
+                ForceDeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
